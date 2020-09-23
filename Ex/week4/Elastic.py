909bfe840fe54e20931f84c81d798397c620ced1
ehdqoddl6 <hays39@naver.com>
2020-09-24 02:05:08 +0900

    ,

--- Ex/week4/Elastic.py
+++ Ex/week4/Elastic.py
@@ -28,7 +28,6 @@ MSE5 = cross_val_score(lin_reg, x5, y,  scoring='neg_mean_squared_error', cv=5)
 mean_MSE = np.mean(MSE5)  
 print('linear regreesion : ')
 print(mean_MSE)
-print('\n')
 
 
 from sklearn.model_selection import GridSearchCV  
@@ -36,6 +35,7 @@ from sklearn.linear_model import Ridge
 from sklearn.model_selection import KFold
 
 
+print('-----------------------')
 print('ridge')
 ridge = Ridge()
 parameters = {'alpha': [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2,  1, 5, 10, 20]}
@@ -43,11 +43,14 @@ parameters = {'alpha': [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2,  1, 5, 10, 20]}
 ridge_regressor = GridSearchCV(ridge, parameters, scoring='neg_mean_squared_error', cv=10)  
 ridge_regressor.fit(x5, y)
 
-print(ridge_regressor.best_params_)  
-print(ridge_regressor.best_score_)
-print('\n')
+for i in range(len(ridge_regressor.cv_results_["params"])):
+    print(ridge_regressor.cv_results_["params"][i] , "Accuracy : ", ridge_regressor.cv_results_["mean_test_score"][i])
+
+print('Best parameter : ', ridge_regressor.best_params_)  
+print('Best accuracy : ', ridge_regressor.best_score_)
 
 
+print('-----------------------')
 print('Lasso')
 from sklearn.linear_model import Lasso 
  
@@ -56,13 +59,15 @@ parameters = {'alpha': [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2,  1, 5, 10, 20]}
 lasso_regressor = GridSearchCV(lasso, parameters, scoring='neg_mean_squared_error', cv=10)  
 lasso_regressor.fit(x5, y)
 
+for i in range(len(lasso_regressor.cv_results_["params"])):
+    print(lasso_regressor.cv_results_["params"][i] , "Accuracy : ", lasso_regressor.cv_results_["mean_test_score"][i])
 
-print(lasso_regressor.best_params_)  
-print(lasso_regressor.best_score_)
-print('\n')
+print('Best parameter : ',lasso_regressor.best_params_)  
+print('Best accuracy : ',lasso_regressor.best_score_)
 
 
 
+print('-----------------------')
 print('ElasticNet')
 from sklearn.linear_model import ElasticNet   
 
@@ -70,8 +75,11 @@ elast = ElasticNet();
 parameters = {'alpha': [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2,  1, 5, 10, 20], 'l1_ratio': [0.1, 0.3, 0.5, 0.7, 0.9]}
 elast_regressor = GridSearchCV(elast, parameters, scoring='neg_mean_squared_error', cv=10)
 elast_regressor.fit(x5, y)
+
+for i in range(len(elast_regressor.cv_results_["params"])):
+    print(elast_regressor.cv_results_["params"][i] , "Accuracy : ", elast_regressor.cv_results_["mean_test_score"][i])
  
-print(elast_regressor.best_params_)  
-print(elast_regressor.best_score_)
+print('Best parameter : ',elast_regressor.best_params_)  
+print('Best accuracy : ',elast_regressor.best_score_)
 
 
