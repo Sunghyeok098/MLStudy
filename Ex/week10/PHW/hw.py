10d960ace59f8c22eb6cadf15647a2ad70934f91
ehdqoddl6 <hays39@naver.com>
2020-11-05 22:15:35 +0900

    .

--- Ex/week10/PHW/hw.py
+++ Ex/week10/PHW/hw.py
@@ -13,48 +13,148 @@ from sklearn.model_selection import GridSearchCV
 data_url = 'C:/Users/hays3/git/MLStudy/Ex/dataSet/week10_dataSet\mushrooms.csv'
 data = pd.read_csv(data_url)
 
+data.replace({'?':np.nan}, inplace=True)
+data = data.fillna(method='ffill')
+
+
+
 from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
 
 le = LabelEncoder()
-
 column = data.columns
 
-
-
-for i in column:
+for i in column:   
     data[i] = le.fit_transform(data[i])
 
-print(data)
-
-"""
-data['buying'] = le.fit_transform(data['buying'])
-data['maint'] = le.fit_transform(data['maint'])
-data['doors'] = le.fit_transform(data['doors'])
-data['persons'] = le.fit_transform(data['persons'])
-data['lug_boot'] = le.fit_transform(data['lug_boot'])
-data['safety'] = le.fit_transform(data['safety'])
-data['car'] = le.fit_transform(data['car'])
-
+x = data.drop(['class'], axis=1)
+y = data['class']
 
 from sklearn.cluster import DBSCAN
 
-eps = [0.001, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5]
-min_samples = [3, 5, 10, 15, 20, 30, 50, 100]
+
+"""
+eps = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
+min_samples = [5, 10, 20, 50, 100]
+algorithm = ['ball_tree', 'kd_tree', 'brute']
 
 for i in eps:
     for j in min_samples:
+        for k in algorithm:
 
-        model = DBSCAN(eps=i, min_samples=j)
+            model = DBSCAN(eps=i, min_samples=j, algorithm=k, metric='euclidean', p=2)
+            predict = model.fit_predict(x)
 
-        predict = pd.DataFrame(model.fit_predict(data))
-        predict.columns=['predict']
-        print(np.unique(predict))
+            sum = 0
 
-        r = pd.concat([feature, predict],axis=1)
+            for l in np.unique(predict):
 
-        fig = plt.figure( figsize=(6,6))
-        plt.scatter(r['x'], r['y'], c=r['predict'],alpha=0.5)
-        plt.title('dbscan eps : {eps}, min_samples {min_samples}' .format(eps=i, min_samples=j))
-        plt.show()
+                count0 = 0
+                count1 = 0
+
+                for o in range(len(y)):
+
+                    if(predict[o] == l):
+
+                        if(y[o] == 0):
+                            count0 += 1
+                        else:
+                            count1 += 1
+                
+                if(count0 > count1):
+                    sum = sum + count0
+                else:
+                    sum = sum + count1
+                
+                print('dbscan eps : {eps}, min_samples {min_samples}, algorithm {algorithm}' .format(eps=i, min_samples=j, algorithm=k))
+                print('Purity : ', round(sum/len(y), 2))
+
+            
+
+
+
+
+x = data.drop(['class'], axis=1)
+y = data['class']
+
+scaler = preprocessing.MinMaxScaler(feature_range=(0, 10)) 
+x = scaler.fit_transform(x) 
+
+eps = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
+min_samples = [5, 10, 20, 50, 100]
+algorithm = ['ball_tree', 'kd_tree', 'brute']
+
+for i in eps:
+    for j in min_samples:
+        for k in algorithm:
+
+            model = DBSCAN(eps=i, min_samples=j, algorithm=k, metric='euclidean', p=2)
+            predict = model.fit_predict(x)
+
+            sum = 0
+
+            for l in np.unique(predict):
+
+                count0 = 0
+                count1 = 0
+
+                for o in range(len(y)):
+
+                    if(predict[o] == l):
+
+                        if(y[o] == 0):
+                            count0 += 1
+                        else:
+                            count1 += 1
+                
+                if(count0 > count1):
+                    sum = sum + count0
+                else:
+                    sum = sum + count1
+                
+                print('dbscan eps : {eps}, min_samples {min_samples}, algorithm {algorithm}' .format(eps=i, min_samples=j, algorithm=k))
+                print('Purity : ', round(sum/len(y), 2))
+
 
 """
+x = data.drop(['class'], axis=1)
+y = data['class']
+
+scaler = preprocessing.StandardScaler()
+x = scaler.fit_transform(x) 
+
+eps = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
+min_samples = [5, 10, 20, 50, 100]
+algorithm = ['ball_tree', 'kd_tree', 'brute']
+
+for i in eps:
+    for j in min_samples:
+        for k in algorithm:
+
+            model = DBSCAN(eps=i, min_samples=j, algorithm=k, metric='euclidean', p=2)
+            predict = model.fit_predict(x)
+
+            sum = 0
+
+            for l in np.unique(predict):
+
+                count0 = 0
+                count1 = 0
+
+                for o in range(len(y)):
+
+                    if(predict[o] == l):
+
+                        if(y[o] == 0):
+                            count0 += 1
+                        else:
+                            count1 += 1
+                
+                if(count0 > count1):
+                    sum = sum + count0
+                else:
+                    sum = sum + count1
+                
+                print('dbscan eps : {eps}, min_samples {min_samples}, algorithm {algorithm}' .format(eps=i, min_samples=j, algorithm=k))
+                print('Purity : ', round(sum/len(y), 2))
+            
