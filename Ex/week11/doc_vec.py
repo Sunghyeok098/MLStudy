a8578255ce90dcdfcd46e7269af12701da82b932
Sunghyeok Ha <hays39@naver.com>
2020-11-13 17:46:02 +0900

    .

--- Ex/week11/doc_vec.py
+++ Ex/week11/doc_vec.py
@@ -34,3 +34,62 @@ print(sentences)
 vectorizer = TfidfVectorizer()
 # fit the model to train data to create a normalized TF-IDF
 
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+import numpy as np
+f = open("resources/d1.txt", 'r')
+data1 = f.read()
+print(data1)
+
+f = open("resources/d2.txt", 'r')
+data2 = f.read()
+print(data2)
+
+f = open("resources/d3.txt", 'r')
+data3 = f.read()
+print(data3)
+
+f = open("resources/d4.txt", 'r')
+data4 = f.read()
+print(data4)
+
+f = open("resources/d5.txt", 'r')
+data5 = f.read()
+print(data5)
+
+f.close()
+
+tfidf_vectorizer = TfidfVectorizer()
+
+query = ["corona vaccine fake news"]
+data1 = [data1]
+data2 = [data2]
+data3 = [data3]
+data4 = [data4]
+data5 = [data5]
+
+text = query + data1 + data2 + data3 + data4 + data5
+
+print(text)
+
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf_vectorizer.fit_transform(text)
+#Print Vocabulary
+print(tfidf_vectorizer.vocabulary_)
+#shape print word frequently
+print(tfidf_matrix.shape)
+#TF-IDF TABLE
+print(tfidf_matrix.toarray())
+
+from sklearn.metrics.pairwise import linear_kernel
+#cosine similarity
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+print(cosine_sim)
+data = pd.DataFrame(cosine_sim,columns=["Q","D1","D2","D3","D4","D5"]);
+data2 = data.iloc[[0],:]
+data2 = data2.iloc[:,1:]
+#rank order
+print(data2.rank(method='max', ascending=False, axis=1))
--- TP/code/clf_analysis.py
+++ TP/code/clf_analysis.py
@@ -7,6 +7,7 @@ from sklearn.metrics import classification_report,  confusion_matrix
 from sklearn.model_selection import train_test_split, GridSearchCV
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib import style
+from sklearn import metrics
 
 import warnings
 warnings.filterwarnings(action='ignore')
@@ -16,9 +17,6 @@ pd.set_option('display.max_columns', 100)
 data_url = 'TP/dataset/classification/labeling_data.csv'
 data = pd.read_csv(data_url)
 
-
-print("\nStart RandomForest")
-
 data_train, data_test = train_test_split(data, test_size=0.2)
 
 x_train = data_train.drop(['Churn'], axis=1)
@@ -27,50 +25,128 @@ x_test = data_test.drop(['Churn'], axis=1)
 y_test = data_test['Churn']
 
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
+print("\nStart LogisticRegression")
+from sklearn.linear_model import LogisticRegression
 
-forest = RandomForestClassifier()
-
-param_grid = {'criterion':['gini', 'entropy'], 'n_estimators':[1, 10, 100], 'max_depth':[1, 10, 100]}
-forest_gscv = GridSearchCV(forest, param_grid, cv=5)
-forest_gscv.fit(x_train, y_train)
-forest_gscv.predict(x_test)
-
+logisticRegr = LogisticRegression()
+param_grid = {'C':[0.1, 1, 10], 'solver':['liblinear', 'lbfgs', 'sag'], 'max_iter':[50, 100, 200]}
+logisticRegr_gscv = GridSearchCV(logisticRegr, param_grid, cv=5)
+logisticRegr_gscv.fit(x_train, y_train)
+logisticRegr_gscv.predict(x_test)
 
 
-for i in range(len(forest_gscv.cv_results_["params"])):
-    print(forest_gscv.cv_results_["params"][i] , "Accuracy : ", forest_gscv.cv_results_["mean_test_score"][i])
+for i in range(len(logisticRegr_gscv.cv_results_["params"])):
+    print(logisticRegr_gscv.cv_results_["params"][i] , "Accuracy : ", logisticRegr_gscv.cv_results_["mean_test_score"][i])
 
 print("\n")
-print("Best paramter : ", forest_gscv.best_params_)
-print("Best accuracy : ", forest_gscv.best_score_)
-print(confusion_matrix(y_test,forest_gscv.predict(x_test)))  
+print("Best parameter : ",logisticRegr_gscv.best_params_)
+print("Best accuracy : ", logisticRegr_gscv.best_score_)
+print(confusion_matrix(y_test,logisticRegr_gscv.predict(x_test))) 
 plt.figure(figsize=(2, 2))
-sns.heatmap(metrics.confusion_matrix(y_test,forest_gscv.predict(x_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+sns.heatmap(metrics.confusion_matrix(y_test,logisticRegr_gscv.predict(x_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
 
 
-nEst_gini = []
-mDep_gini = []
-value_gini = []
-nEst_entro = []
-mDep_entro = []
-value_entro = []
 
-value = np.array(forest_gscv.cv_results_["mean_test_score"]).reshape(-1, 3)
-print(value)
+print("\nStart SVM")
+from sklearn.svm import SVC
 
-for i in range(len(forest_gscv.cv_results_["params"])):
-    param1 = forest_gscv.cv_results_["params"][i]['criterion']
-    param2 = forest_gscv.cv_results_["params"][i]['n_estimators']
-    param3 = forest_gscv.cv_results_["params"][i]['max_depth']
-    
-    if(param1 == 'gini'):
-        nEst_gini.append(param2)
-        mDep_gini.append(param3)
-        value_gini.append(forest_gscv.cv_results_["mean_test_score"][i])
+svclassifier = SVC()
+param_grid = {'C':[0.1, 1, 10], 'kernel':['linear', 'rbf', 'sigmoid'], 'gamma':[0.01, 0.1, 1.0, 10.0]}
+svclassifier_gscv = GridSearchCV(svclassifier, param_grid, cv=5)
+svclassifier_gscv.fit(x_train, y_train)
+svclassifier_gscv.predict(x_test)
 
-    else:
-        nEst_entro.append(param2)
-        mDep_entro.append(param3)
-        value_entro.append(forest_gscv.cv_results_["mean_test_score"][i])
+
+for i in range(len(svclassifier_gscv.cv_results_["params"])):
+    print(svclassifier_gscv.cv_results_["params"][i] , "Accuracy : ", svclassifier_gscv.cv_results_["mean_test_score"][i])
+
+print("\n")
+print("Best parmeter : ",svclassifier_gscv.best_params_)
+print("Best accuracy : ", svclassifier_gscv.best_score_)    
+print(confusion_matrix(y_test,svclassifier_gscv.predict(x_test)))  
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(y_test,svclassifier_gscv.predict(x_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knnclassifier = KNeighborsClassifier()
+param_grid = {'algorithm':['ball_tree', 'kd_tree', 'brute'], 'n_neighbors':[2,3,4,5], 'weights':['uniform', 'distance']}
+knnclassifier_gscv = GridSearchCV(knnclassifier, param_grid, cv=5)
+knnclassifier_gscv.fit(x_train, y_train)
+knnclassifier_gscv.predict(x_test)
+
+
+for i in range(len(svclassifier_gscv.cv_results_["params"])):
+    print(svclassifier_gscv.cv_results_["params"][i] , "Accuracy : ", svclassifier_gscv.cv_results_["mean_test_score"][i])
+
+print("\n")
+print("Best parmeter : ", knnclassifier_gscv.best_params_)
+print("Best accuracy : ", knnclassifier_gscv.best_score_)    
+print(confusion_matrix(y_test, knnclassifier_gscv.predict(x_test)))  
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(y_test, knnclassifier_gscv.predict(x_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+
+
+
+print("\nStart DecisionTree")
+from sklearn.tree import DecisionTreeClassifier
+
+deClassifier = DecisionTreeClassifier()
+param_grid = {'criterion':['gini', 'entropy'], 'max_depth':[5, 10, 15, 20]}
+deClassifier_gscv = GridSearchCV(deClassifier, param_grid, cv=5)
+deClassifier_gscv.fit(x_train, y_train)
+deClassifier_gscv.predict(x_test)
+
+
+for i in range(len(deClassifier_gscv.cv_results_["params"])):
+    print(deClassifier_gscv.cv_results_["params"][i] , "Accuracy : ", deClassifier_gscv.cv_results_["mean_test_score"][i])
+
+print("\n")
+print("Best parmeter : ", deClassifier_gscv.best_params_)
+print("Best accuracy : ", deClassifier_gscv.best_score_)    
+print(confusion_matrix(y_test, deClassifier_gscv.predict(x_test)))  
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(y_test, deClassifier_gscv.predict(x_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+
+
+print("\nStart RandomForest")
+
+from sklearn.ensemble import RandomForestClassifier
+
+forestClassifier = RandomForestClassifier()
+
+param_grid = {'criterion':['gini', 'entropy'], 'n_estimators':[1, 10, 100], 'max_depth':[1, 10, 100]}
+forestClassifier_gscv = GridSearchCV(forestClassifier, param_grid, cv=5)
+forestClassifier_gscv.fit(x_train, y_train)
+forestClassifier_gscv.predict(x_test)
+
+for i in range(len(forestClassifier_gscv.cv_results_["params"])):
+    print(forestClassifier_gscv.cv_results_["params"][i] , "Accuracy : ", forestClassifier_gscv.cv_results_["mean_test_score"][i])
+
+print("\n")
+print("Best paramter : ", forestClassifier_gscv.best_params_)
+print("Best accuracy : ", forestClassifier_gscv.best_score_)
+print(confusion_matrix(y_test,forestClassifier_gscv.predict(x_test)))  
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(y_test,forestClassifier_gscv.predict(x_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+
+
+from sklearn.ensemble import VotingClassifier
+
+voteClassifier = VotingClassifier()
+
+param_grid = {'voting':['hard', 'soft']}
+voteClassifier_gscv = GridSearchCV(voteClassifier, param_grid, cv=5)
+voteClassifier_gscv.fit(x_train, y_train)
+voteClassifier_gscv.predict(x_test)
+
+for i in range(len(voteClassifier_gscv.cv_results_["params"])):
+    print(voteClassifier_gscv.cv_results_["params"][i] , "Accuracy : ", voteClassifier_gscv.cv_results_["mean_test_score"][i])
+
+print("\n")
+print("Best paramter : ", voteClassifier_gscv.best_params_)
+print("Best accuracy : ", voteClassifier_gscv.best_score_)
+print(confusion_matrix(y_test, voteClassifier_gscv.predict(x_test)))  
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(y_test, voteClassifier_gscv.predict(x_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
